a6448615301b4cdb2e5c28ebc154e11c01b34553
Yana <greedygoga@gmail.com>
2023-10-02 19:38:46 +0300

    Initial commit

---
+++ src/ModelElements/Flash.java
@@ -0,0 +1,20 @@
+package ModelElements;
+
+
+import Staff.Angle3D;
+import Staff.Point3D;
+
+public class Flash {
+    public Point3D Location;
+    public Angle3D Angle;
+    public java.awt.Color color;
+    public float Power;
+
+    public void Rotate(Angle3D angleAction) {
+
+    }
+
+    public void Move(Point3D pointAction){
+
+    }
+}
