d4df21a9fa5b9842b93a9f0c99c03b1f094553ba
Yana <greedygoga@gmail.com>
2023-10-05 18:30:37 +0300

    Added new files

---
+++ src/HW1/InMemoryModel/ModelStore.java
@@ -0,0 +1,48 @@
+package InMemoryModel;
+
+
+import ModelElements.Camera;
+import ModelElements.Flash;
+import ModelElements.PoligonalModel;
+import ModelElements.Scene;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelStore implements IModelChanger {
+    public List<PoligonalModel> Models;
+    public List<Scene> Scenes;
+    public List<Flash> Flashes;
+    public List<Camera> Cameras;
+    private List<IModelChangeObserver> ChangeObservers;
+
+    public ModelStore(List<IModelChangeObserver> changeObservers) throws Exception {
+        this.ChangeObservers = changeObservers;
+
+        this.Models = new ArrayList<>();
+        this.Scenes = new ArrayList<>();
+        this.Flashes = new ArrayList<>();
+        this.Cameras = new ArrayList<>();
+
+        Models.add(new PoligonalModel(null));
+        Flashes.add(new Flash());
+        Cameras.add(new Camera());
+        Scenes.add(new Scene(0,Models,Flashes, Cameras));
+    }
+
+
+    public Scene GetScena(int ID) {
+        for (int i = 0; i < Scenes.size(); i++) {
+            if (Scenes.get(i).ID == ID) {
+                return Scenes.get(i);
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public void NotifyChange(IModelChanger sender) {
+
+    }
+}
---
+++ src/HW1/ModelElements/Poligon.java
@@ -0,0 +1,15 @@
+package ModelElements;
+
+
+import Staff.Point3D;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Poligon {
+    public List<Point3D> points = new ArrayList<Point3D>();
+
+    public Poligon(Point3D point) {
+        points.add(point);
+    }
+}
